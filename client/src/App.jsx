c7effe56ffba7e8809624110634ce3d315808cfb
Muskaan <muskaanchaturvedi111@gmail.com>
2023-11-03 15:53:02 +0530

    base url updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,7 +13,7 @@ import PlaceDetailPage from './pages/PlaceDetailPage'
 import BookingsPage from './pages/BookingsPage'
 import BookingsPageSingle from './pages/BookingPageSingle'
 
-axios.defaults.baseURL = 'http://localhost:4000';
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
 axios.defaults.withCredentials = true;
 
 function App() {
